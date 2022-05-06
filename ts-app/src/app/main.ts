13405c384e2a12ba91abcd994e63c00612f93847
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2022-05-05 22:03:58 -0500

    Readonly Type

--- ts-app/src/app/main.ts
+++ ts-app/src/app/main.ts
@@ -1,16 +1,16 @@
 import faker from '@faker-js/faker';
 
-import { addProduct, products, updateProduct } from './products/product.service';
+import { addProduct, products, updateProduct, findProducts } from './products/product.service';
 
-for (let index = 0; index < 50; index++) {
+for (let index = 0; index < 5; index++) {
   addProduct({
     description: faker.commerce.productDescription(),
     image: faker.image.imageUrl(),
     color: faker.commerce.color(),
-    size: faker.random.arrayElement(['M', 'S', 'XL', 'L']),
+    size: faker.helpers.arrayElement(['M', 'S', 'XL', 'L']),
     price: parseInt(faker.commerce.price(), 10),
     isNew: faker.datatype.boolean(),
-    tags: faker.random.arrayElements(),
+    tags: faker.helpers.arrayElements(),
     title: faker.commerce.productName(),
     stock: faker.datatype.number({min: 10, max: 100}),
     categoryId: faker.datatype.uuid()
@@ -25,3 +25,10 @@ updateProduct(product.id, {
   title: 'New title',
   stock: 80,
 });
+
+findProducts({
+  stock: 10,
+  color: 'red',
+  createdAt: new Date(),
+  isNew: true,
+})
